5d12d253d4e2042619c436be10a77441879b8409
Unknown <trolldiego@hotmail.es>
2018-03-21 19:42:03 -0500

    Ejercicio 5

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+        int numero=0,i=0,multiplicacion=0,suma=0,suma1=0,fin=0;
+        i=0;
+            while (i<=100){
+            multiplicacion=i*i+1;
+
+                cout << i << endl;
+
+            i++;
+
+
+                if (i%2){
+
+
+                    cout << "Numero impar  ";
+
+                }
+
+                    else {
+
+
+                        cout << "Numero par    ";
+
+
+                        }
+
+
+                            cout << "La multiplicacion es : -- "<<multiplicacion<<endl;
+
+
+            }
+
+
+
+
+
+
+
+    return 0;
+
+}
